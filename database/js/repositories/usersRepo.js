e3bc396248210628311264a1c1bdb9d6aab868b0
Skjaldbaka17 <Skjaldbaka17@gmail.com>
2020-08-01 19:25:33 +0000

    Failed at postgres, value not = when inserting

--- database/js/repositories/usersRepo.js
+++ database/js/repositories/usersRepo.js
@@ -6,6 +6,8 @@ const {
 } = process.env
 
 const getUserByEmail = async (email) => {
+    console.log("GETTING USER BY EMAIL")
+    console.log(`SELECT * FROM ${DB_USERS} where email = '${email.toLowerCase()}'`)
     const usersQuery = `SELECT * FROM ${DB_USERS} where email = $1`
     const result = await query(usersQuery, [email.toLowerCase()])
     return result
@@ -14,7 +16,8 @@ const getUserByEmail = async (email) => {
 
 const insertUser = async (user = {email, uuid, password}) => {
     console.log("USER: ", user)
-    const usersQuery = `INSERT INTO ${DB_USERS} (email, uuid, password) = ($1,$2,$3) returning *`
+    console.log(`INSERT INTO ${DB_USERS} (email, uuid, password) = ('${user.email.toLowerCase()}', ${user.uuid}, ${user.password}) returning *`)
+    const usersQuery = `INSERT INTO ${DB_USERS} (email, uuid, password)  VALUES ($1,$2,$3) returning *`
     const result = await query(usersQuery, [user.email.toLowerCase(), user.uuid, user.password])
     return result
 }
