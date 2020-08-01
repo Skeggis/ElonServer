c42d7e617ae80f77f979e0eb910e91c2df1e9ace
Skjaldbaka17 <Skjaldbaka17@gmail.com>
2020-08-01 19:12:45 +0000

    Query fail

--- database/js/repositories/usersRepo.js
+++ database/js/repositories/usersRepo.js
@@ -6,15 +6,15 @@ const {
 } = process.env
 
 const getUserByEmail = async (email) => {
-    const query = `SELECT * FROM ${DB_USERS} where email = $1`
-    const result = await query(query, [email.toLowerCase()])
+    const usersQuery = `SELECT * FROM ${DB_USERS} where email = $1`
+    const result = await query(usersQuery, [email.toLowerCase()])
     return result
 }
 
 
 const insertUser = async (user = {email, uuid, password}) => {
-    const query = `INSERT INTO ${DB_USERS} (email, uuid, password) = ($1,$2,$3) returning *`
-    const result = await query(query, [user.email.toLowerCase(), user.uuid, user.password])
+    const usersQuery = `INSERT INTO ${DB_USERS} (email, uuid, password) = ($1,$2,$3) returning *`
+    const result = await query(usersQuery, [user.email.toLowerCase(), user.uuid, user.password])
     return result
 }
 
