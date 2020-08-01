f209a9c5c470546e3a9f21583f9c760c7f05e015
Skjaldbaka17 <Skjaldbaka17@gmail.com>
2020-08-01 18:50:19 +0000

    Fail

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use('/programs', programRouter)
-app.user('/users', usersRouter)
+app.use('/users', usersRouter)
 app.use(notFoundHandler)
 app.use(errorHandler)
 
