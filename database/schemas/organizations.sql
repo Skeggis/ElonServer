1a805bb84c7dc35af5b7135ca4ed6a4b02b7afeb
Skjaldbaka17 <Skjaldbaka17@gmail.com>
2020-08-10 18:27:11 +0000

    Removed reference to owner_id in organization.sql schema

--- database/schemas/organizations.sql
+++ database/schemas/organizations.sql
@@ -1,7 +1,7 @@
 create table organizations (
   id serial primary key,
   name varchar(255) not null,
-  owner_id uuid REFERENCES users(uuid) not null,
+  owner_id uuid not null,
   image_url varchar(255),
   created timestamp default current_timestamp
 );
