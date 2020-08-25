be250a34ff3e1caf180fff66e6f5165c4b3bfc69
Róbert Ingi Huldarsson <robert.huldarsson@gmail.com>
2020-08-25 14:07:44 +0000

    await

--- handlers/usersHandler.js
+++ handlers/usersHandler.js
@@ -89,7 +89,7 @@ const getUSerByUUIDHandler = async (UUID) => {
 
     const joinResult = await getRequestToJoinOrganizationFromUUID(UUID)
     if (joinResult.rows.length > 0) {
-        result.rows[0].requestOrganization = getOrganizationFromId(joinResult.rows[0].organization_id)
+        result.rows[0].requestOrganization = await getOrganizationFromId(joinResult.rows[0].organization_id)
     }
 
     return {
